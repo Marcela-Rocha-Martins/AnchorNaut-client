5e0b8b6e4c8f9f47d115f3d7a8a61288d00f8dd2
marcela rocha <marcelarochamartins@gmail.com>
2023-07-19 22:05:36 +0200

    just the starter code

--- src/pages/EditProjectPage.jsx
+++ src/pages/EditProjectPage.jsx
@@ -1,19 +1,27 @@
 import { useState, useEffect } from "react";
-import { useParams, useNavigate } from 'react-router-dom';
 import axios from "axios";
+import { useNavigate, useParams } from 'react-router-dom';
 
 const API_URL = "http://localhost:5005";
 
 function EditProjectPage(props) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  
+
+  const navigate =  useNavigate();
   const { projectId } = useParams();
-  const navigate = useNavigate();
+  
   
   useEffect(() => {
+    // Get the token from the localStorage
+    const storedToken = localStorage.getItem('authToken');
+    
+    // Send the token through the request "Authorization" Headers 
     axios
-      .get(`${API_URL}/api/projects/${projectId}`)
+      .get(
+        `${API_URL}/api/projects/${projectId}`,
+        { headers: { Authorization: `Bearer ${storedToken}` } }    
+      )
       .then((response) => {
         const oneProject = response.data;
         setTitle(oneProject.title);
@@ -27,9 +35,17 @@ function EditProjectPage(props) {
   const handleFormSubmit = (e) => {
     e.preventDefault();
     const requestBody = { title, description };
+  
+    // Get the token from the localStorage
+    const storedToken = localStorage.getItem('authToken');  
 
+    // Send the token through the request "Authorization" Headers   
     axios
-      .put(`${API_URL}/api/projects/${projectId}`, requestBody)
+      .put(
+        `${API_URL}/api/projects/${projectId}`,
+        requestBody,
+        { headers: { Authorization: `Bearer ${storedToken}` } }              
+      )
       .then((response) => {
         navigate(`/projects/${projectId}`)
       });
@@ -37,12 +53,16 @@ function EditProjectPage(props) {
   
   
   const deleteProject = () => {
+    // Get the token from the localStorage
+    const storedToken = localStorage.getItem('authToken');      
     
+    // Send the token through the request "Authorization" Headers   
     axios
-      .delete(`${API_URL}/api/projects/${projectId}`)
-      .then(() => {
-        navigate("/projects");
-      })
+      .delete(
+        `${API_URL}/api/projects/${projectId}`,
+        { headers: { Authorization: `Bearer ${storedToken}` } }           
+      )
+      .then(() => navigate("/projects"))
       .catch((err) => console.log(err));
   };  
 
--- src/pages/ProjectDetailsPage.jsx
+++ src/pages/ProjectDetailsPage.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
-import { useParams, Link } from 'react-router-dom';
 import axios from "axios";
+import { Link, useParams } from "react-router-dom";
 import AddTask from "../components/AddTask";
-import TaskCard from "../components/TaskCard";
 
+import TaskCard from "../components/TaskCard";
 
 const API_URL = "http://localhost:5005";
 
@@ -12,13 +12,21 @@ function ProjectDetailsPage (props) {
   const [project, setProject] = useState(null);
   const { projectId } = useParams();
   
+  
   const getProject = () => {
+    // Get the token from the localStorage
+    const storedToken = localStorage.getItem('authToken');
+
+    // Send the token through the request "Authorization" Headers
     axios
-      .get(`${API_URL}/api/projects/${projectId}`)
+      .get(
+        `${API_URL}/api/projects/${projectId}`,
+        { headers: { Authorization: `Bearer ${storedToken}` } }
+      )
       .then((response) => {
-      	const oneProject = response.data;
-      	setProject(oneProject);
-    	})
+        const oneProject = response.data;
+        setProject(oneProject);
+      })
       .catch((error) => console.log(error));
   };
   
@@ -30,7 +38,6 @@ function ProjectDetailsPage (props) {
   
   return (
     <div className="ProjectDetails">
-    
       {project && (
         <>
           <h1>{project.title}</h1>
